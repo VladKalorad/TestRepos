94ec60c2dda150c8c07cb42f0a1b8e5721b85b72
cherniayskivlad <vladchernav210@gmail.com>
2022-02-17 08:09:26 +0300

    laba5

---
+++ kpiayp5/kpiayp5/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kpiayp5
+{
+    class Program
+    {
+        static int Nod(int a, int b)
+        {
+            int x = 0;
+            if (a<b)
+            {
+                x = a;
+            }
+            else
+            {
+                x = b;
+            }
+            for (int i = x; i > 1; i--)
+                if (((a % i) == 0) & ((b % i) == 0))
+                {
+                    return i;
+                }
+            return 1;
+        }
+        static void Check(int[]arr)
+        {
+            int[] newarr = new int[2];
+            int nok = arr[0];
+            for(int i =0; i < arr.Length;++i)
+            { 
+                nok = (nok * arr[i]) / Nod(nok, arr[i]);
+                newarr[0] = Nod(nok, arr[i]);
+                newarr[1] = nok;
+            }
+            for (int j = 0; j < newarr.Length; j++)
+            {
+                Console.WriteLine($"Новый массив:{newarr[j]}");
+            }
+            Console.WriteLine($"Нок = {nok}");
+
+        }
+        public static void InputArr(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public static int FirstEven(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    return arr[i];
+                   
+                }
+            }
+            return 0;
+        }
+
+        public static void PlusPlus(int[] arr)
+        {
+            int num = FirstEven(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    arr[i] += num;
+                }
+                Console.WriteLine(arr[i]); ;
+            }
+        }
+        static void PrintArr(int[]arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите элементы массива: ");
+            int[] arr = new int[n];
+            InputArr(arr);
+            //Console.WriteLine(FirstEven(arr));
+            //PlusPlus(arr);
+            Check(arr);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ kpiayp5/kpiayp5/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("kpiayp5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("kpiayp5")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7ee7df1e-ac5c-41b7-b86d-8a29b81c8cc4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
